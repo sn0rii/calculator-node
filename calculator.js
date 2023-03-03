3dc611f6184a917b55ff3394dbd64fae733d7a92
sn0rii <113938479+sn0rii@users.noreply.github.com>
2023-03-03 06:56:00 +0100

    update

--- calculator.js
+++ calculator.js
@@ -8,8 +8,6 @@ app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
 });
 
-
-
 app.post("/", function(req, res) {
 
 var num1 = Number(req.body.num1);
@@ -17,9 +15,25 @@ var num2 = Number(req.body.num2);
 
 var result = num1 + num2;
 
-   res.send("The resoult of the calculator is " + result) 
+   res.send("The result of the calculator is " + result) 
 });
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
 });
+
+
+
+app.get("/bmicalculator", function(req, res){
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmiCalculator", function(req, res) {
+
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+    
+    var bmi = weight / (height * height);
+    
+       res.send("Your BMI is " + bmi) 
+    });
